e9766ed6c7d4bdc1dd70da4c8c8c016467f7cd75
Irakli Svanidze <isvanidze@molhsa.local>
2017-12-24 19:31:48 +0400

    init

---
+++ api/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+
+  extends: 'standard',
+
+  // add your custom rules here
+  'rules': {
+    'no-console': 0,
+    'no-unused-expressions': 0,
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'prefer-destructuring': 0,
+    'object-shorthand': 0,
+    'no-use-before-define': 0,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 0,
+    'no-return-await': 0,
+    'no-plusplus': 0
+
+  }
+}
---
+++ public/.eslintrc.js
@@ -0,0 +1,37 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+    'no-console': 0,
+    'no-unused-expressions': 0,
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'prefer-destructuring': 0,
+    'object-shorthand': 0,
+    'no-use-before-define': 0,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 0,
+    'no-return-await': 0,
+    'no-plusplus': 0
+  }
+}
