744290e31d9bba9cb05ae81f3fd7b2fce1cfdf7b
irakli jiishkariani <irakli2692@gmail.com>
2017-12-26 12:52:02 +0400

    express template

--- api/app.js
+++ api/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(express.static('../public/dist'))
 
 app.get('*', (req, res) => res.redirect('/'))
