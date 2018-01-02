803f2496cf4159abc974ada03c86490f2de2ccda
Irakli Svanidze <isvanidze@molhsa.local>
2018-01-02 18:16:25 +0400

    skills component view

---
+++ public/src/components/profile.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <ul>
+      <li v-for="item in skillList">{{item}}</li>
+    </ul>
+    <skills :editable="true" :list="skillList" @onAddNewSkill="onAddNewSkill" @onRemoveSkill="onRemoveSkill"></skills>
+
+
+
+  </div>
+
+</template>
+
+<script>
+  import skills from './skills'
+
+  export default {
+    name: 'profile',
+    data() {
+      return {
+        msg: 'profile page',
+        skillList: ['javascript', 'nodejs', 'css', 'etc']
+      }
+    },
+    methods: {
+      onAddNewSkill(skill) {
+        let indexOfSkill = this.skillList.indexOf(skill)
+        if (indexOfSkill !== -1) {
+          // TODO alert or notify
+          console.log('this skill already exists')
+          return
+        }
+        this.skillList.push(skill)
+      },
+      onRemoveSkill(skill) {
+        let indexOfSkill = this.skillList.indexOf(skill)
+        if (indexOfSkill === -1) {
+          // TODO alert or notify
+          console.log('can\'t find index of skill')
+          return
+        }
+        this.skillList.splice(indexOfSkill, 1)
+      }
+    },
+    components: { 'skills': skills }
+  }
+</script>
+
+
+<style scoped>
+</style>
---
+++ public/src/components/skills.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2>skills component</h2>
+    <div class="skills-container">
+      <b-input-group v-if="editable">
+        <b-form-input type="text" v-model="newSkill" placeholder="მაგ. ანალიტიკოსი"></b-form-input>
+        <b-input-group-button slot="right">
+          <b-btn @click="addSkill(newSkill)">დამატება</b-btn>
+        </b-input-group-button>
+      </b-input-group>
+      <div class="chip" v-for="item in list">
+        {{item}}
+        <span v-if="editable" class="closebtn" @click="removeSkill(item)">&times;</span>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    name: 'skills',
+    props: ['list', 'editable'],
+    data() {
+      return {
+        newSkill: ''
+      }
+    },
+    methods: {
+      addSkill(skill) {
+        this.$emit('onAddNewSkill', skill)
+      },
+      removeSkill(skill) {
+        this.$emit('onRemoveSkill', skill)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .chip {
+    display: inline-block;
+    padding: 0 25px;
+    height: 50px;
+    font-size: 16px;
+    line-height: 50px;
+    border-radius: 25px;
+    background-color: gold;
+    margin: 5px;
+  }
+
+  .closebtn {
+    padding-left: 10px;
+    color: #888;
+    font-weight: bold;
+    float: right;
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  .closebtn:hover {
+    color: #000;
+  }
+</style>
