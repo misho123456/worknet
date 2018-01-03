efb7825f662e8e4078e73e2b5f93411a5e254524
irakli jiishkariani <irakli2692@gmail.com>
2018-01-03 17:15:58 +0400

    error handling express middleware

--- api/app.js
+++ api/app.js
@@ -13,6 +13,10 @@ app.use(express.static('../public/dist'))
 
 app.use(vacancies.baseUrl, vacancies.router)
 
+app.use(function(err, req, res, next) {
+  res.status(500).send(err.message)
+})
+
 app.get('*', (req, res) => res.redirect('/'))
 
 app.listen(3000, () => console.log('started'))
