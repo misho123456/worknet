875420f0bb436075d18293be883aa8ab6bcc51d5
irakli jiishkariani <irakli2692@gmail.com>
2018-01-08 18:13:34 +0400

    vacancies searchArea frontend

---
+++ public/src/components/search-area.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="searchArea">
+  <div class="form-group flex" @keyup.enter="onSearchClick">
+    <input type="text" class="form-control" v-model="query">
+  </div>
+  &nbsp;
+  <b-button class="searchButton" variant="primary" size="" @click="onSearchClick">
+    <i class="fa fa-search fa-2x" aria-hidden="true"></i>
+  </b-button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'search-area',
+  props: ['query'],
+  data: () => ({
+
+  }),
+  methods: {
+    onSearchClick() {
+      this.$emit('search', this.query)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.searchArea {
+  display: flex;
+  flex-direction: row;
+  width: 60%;
+  margin: auto;
+  padding: 20px;
+  vertical-align: center;
+}
+
+.form-group {
+  margin-top: 16px;
+}
+
+.searchButton {
+  border-radius: 50%;
+  margin: 16px;
+  margin-top: 9px;
+  margin-bottom: 9px;
+}
+
+.flex {
+  flex: 1;
+}
+</style>
--- public/src/components/vacancy/vacancies.vue
+++ public/src/components/vacancy/vacancies.vue
@@ -1,5 +1,6 @@
 <template>
 <div id="vacancies">
+  <search-area @search="search"></search-area>
   <b-card class="mb-2 vacancy" v-for="vacancy in vacancies" :key="vacancy.id">
     <div @click="viewVacancy(vacancy.id)">
       <h3 class="card-title">{{vacancy.positionName}}</h3>
@@ -8,7 +9,10 @@
   </b-card>
 </div>
 </template>
+
 <script>
+import searchArea from '../search-area'
+
 const baseUrl = '/api/vacancies'
 
 export default {
@@ -25,10 +29,20 @@ export default {
   methods: {
     viewVacancy(vacancyId) {
       this.$router.push(`/vacancies/${vacancyId}/view`)
+    },
+    search(query) {
+      this.$http.get(baseUrl, {params: {query}})
+        .then(response => {
+          this.vacancies = response.body
+        })
     }
+  },
+  components: {
+    'search-area': searchArea
   }
 }
 </script>
+
 <style scoped>
 .vacancy:hover {
   box-shadow: 0 0 6px rgba(161, 161, 161, 0.89);
