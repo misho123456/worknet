716a82b3a801e71621848be7579e1f11a5b5139d
irakli jiishkariani <irakli2692@gmail.com>
2018-01-09 12:00:22 +0400

    inline searchArea component

--- public/src/components/vacancy/vacancies.vue
+++ public/src/components/vacancy/vacancies.vue
@@ -1,6 +1,14 @@
 <template>
 <div id="vacancies">
-  <search-area @search="search"></search-area>
+  <div class="searchArea">
+    <div class="form-group flex" @keyup.enter="search">
+      <input type="text" class="form-control" v-model="query">
+    </div>
+    &nbsp;
+    <b-button class="searchButton" variant="primary" size="" @click="search">
+      <i class="fa fa-search fa-2x" aria-hidden="true"></i>
+    </b-button>
+  </div>
   <b-card class="mb-2 vacancy" v-for="vacancy in vacancies" :key="vacancy.id">
     <div @click="viewVacancy(vacancy.id)">
       <h3 class="card-title">{{vacancy.positionName}}</h3>
@@ -11,14 +19,13 @@
 </template>
 
 <script>
-import searchArea from '../search-area'
-
 const baseUrl = '/api/vacancies'
 
 export default {
   name: 'vacancies',
   data: () => ({
-    vacancies: []
+    vacancies: [],
+    query: ''
   }),
   created() {
     this.$http.get(baseUrl)
@@ -30,15 +37,12 @@ export default {
     viewVacancy(vacancyId) {
       this.$router.push(`/vacancies/${vacancyId}/view`)
     },
-    search(query) {
-      this.$http.get(baseUrl, {params: {query}})
+    search() {
+      this.$http.get(baseUrl, {params: {query: this.query}})
         .then(response => {
           this.vacancies = response.body
         })
     }
-  },
-  components: {
-    'search-area': searchArea
   }
 }
 </script>
@@ -46,5 +50,25 @@ export default {
 <style scoped>
 .vacancy:hover {
   box-shadow: 0 0 6px rgba(161, 161, 161, 0.89);
+}
+
+.searchArea {
+  display: flex;
+  flex-direction: row;
+  width: 60%;
+  margin: auto;
+  padding: 20px;
+  vertical-align: center;
+}
+
+.form-group {
+  margin-top: 16px;
+}
+
+.searchButton {
+  border-radius: 50%;
+  margin: 16px;
+  margin-top: 9px;
+  margin-bottom: 9px;
 }
 </style>
