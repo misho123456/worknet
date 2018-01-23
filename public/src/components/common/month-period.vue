894a0d2fa60d74c0c1207ca5c8524fb7fc901f35
irakli jiishkariani <irakli2692@gmail.com>
2018-01-23 17:23:48 +0400

    month-period simple. remove watchers and local variable. use props directly

--- public/src/components/common/month-period.vue
+++ public/src/components/common/month-period.vue
@@ -3,14 +3,14 @@
   <b-container>
     <b-row>
       <b-col>
-        <b-form-select :value="currentPeriod.month" @input="onMonthInput" class="mb-3">
+        <b-form-select :value="month" @input="onMonthInput" class="mb-3">
           <option v-for="monthOption in monthOptions">
             {{monthOption}}
           </option>
         </b-form-select>
       </b-col>
       <b-col>
-        <b-form-select :value="currentPeriod.year" @input="onYearInput" class="mb-3">
+        <b-form-select :value="year" @input="onYearInput" class="mb-3">
           <option v-for="yearOption in yearOptions">{{yearOption}}</option>
         </b-form-select>
       </b-col>
@@ -36,38 +36,20 @@ export default {
   },
   data: () => ({
     monthOptions: new Array(12).fill(0).map((item, index) => index + 1),
-    yearOptions: [],
-    currentPeriod: {}
+    yearOptions: []
   }),
   created() {
     let currentYear = new Date().getFullYear()
 
     this.yearOptions = new Array(currentYear - this.minYear + 1).fill(0)
       .map((item, index) => currentYear - index)
-
-    this.currentPeriod = {
-      month: this.month,
-      year: this.year
-    }
   },
   methods: {
     onMonthInput(value) {
-      this.currentPeriod.month = Number(value)
-
-      this.$emit('month', this.currentPeriod.month)
+      this.$emit('month', Number(value))
     },
     onYearInput(value) {
-      this.currentPeriod.year = Number(value)
-
-      this.$emit('year', this.currentPeriod.year)
-    }
-  },
-  watch: {
-    month(value) {
-      this.currentPeriod.month = Number(value)
-    },
-    year(value) {
-      this.currentPeriod.year = Number(value)
+      this.$emit('year', Number(value))
     }
   }
 }
