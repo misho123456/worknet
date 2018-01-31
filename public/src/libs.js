f3b62673d59557c6d3d1d9ae247300348cf8d53e
irakli jiishkariani <irakli2692@gmail.com>
2018-01-31 16:17:26 +0400

    fetch education types and levels in frontend

--- public/src/libs.js
+++ public/src/libs.js
@@ -10,7 +10,33 @@ async function fetchLocationsOfGeorgia() {
   return this.locationsOfGeorgia
 }
 
+/**
+ * განათლების ტიპების ჩატვირთვა.
+ * @return {Array<String>}
+ */
+async function fetchEducationTypes() {
+  if (this.educationTypes) return this.educationTypes
+  let response = await axios.get('api/libs/educationTypes')
+  this.educationTypes = response.data
+  return this.educationTypes
+}
+
+/**
+ * განათლების საფეხურების ჩატვირთვა
+ * @return {Array<String>}
+ */
+async function fetchEducationLevels() {
+  if (this.educationLevels) return this.educationLevels
+  let response = await axios.get('api/libs/educationLevels')
+  this.educationLevels = response.data
+  return this.educationLevels
+}
+
 export default {
   locationsOfGeorgia: undefined,
-  fetchLocationsOfGeorgia
+  educationTypes: undefined,
+  educationLevels: undefined,
+  fetchLocationsOfGeorgia,
+  fetchEducationTypes,
+  fetchEducationLevels
 }
