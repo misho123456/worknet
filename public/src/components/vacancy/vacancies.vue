9753219dbcdfa3f5441f991dc20b65d30d5de3b2
misho123456 <35832312+misho123456@users.noreply.github.com>
2018-04-22 15:17:44 +0400

    Update vacancies.vue

--- public/src/components/vacancy/vacancies.vue
+++ public/src/components/vacancy/vacancies.vue
@@ -1,7 +1,7 @@
 <template>
 <div id="vacancies">
   <b-button  variant="primary" @click="redirectToVacancyAdd">
-      ახალი ვაკანსია3
+      ახალი ვაკანსია4
   </b-button>
   <div class="searchArea">
     <div class="form-group flex" @keyup.enter="search">
